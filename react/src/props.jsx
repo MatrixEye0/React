dc58f037a899974d6c7fc6d0feb82234d18e0201
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-17 16:23:10 +0530

    add notes

--- react/src/props.jsx
+++ react/src/props.jsx
@@ -5,6 +5,7 @@ export default function Props({tm}) { // here we take tm from app .jsx
    //  let arr = ["PBKS Champions ", "DC ", "LSG ", "MI", "CSK", "SRH", "RR", "RCB", "KKR", "GT"];
    // A child should not directly change its props.
    // prop used to pass data. single way comunication only parent to child 
+   //used for comunication between two or many component
     return (
          <Teams tm={tm}></Teams>
     );
