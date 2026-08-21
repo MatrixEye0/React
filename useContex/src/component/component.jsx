1c8acf6b861aed304f5c032d239c39c11ae12747
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-21 22:36:04 +0530

    add notes

--- useContex/src/component/component.jsx
+++ useContex/src/component/component.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useContext } from 'react'
 import { counterContex } from '../contex/contex'
 
+// React context value change hone par context use karne wale components ko re-render karta hai.
+
 export const Component = () => {// if i want props here so we need drill until component not reach. so this problem overcome with help of useContext
  const counter = useContext(counterContex)// useContext contex se data leta hai.
     return (
