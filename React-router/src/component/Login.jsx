6601c11e9fe7cf40f8324a179d4defb28c2bed38
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-22 16:57:40 +0530

    use Location hook practice

--- React-router/src/component/Login.jsx
+++ React-router/src/component/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export const Login = () => {
+const location = useLocation(); //it is a hook used to get information about the current URL/location. provide as object
+console.log(location);
+console.log(location.pathname)
+console.log(location.hash)
+console.log(location.search)
+console.log(location.mask)
+console.log(location.state)
+
   return (
-    <div>Login</div>
+    <>
+    <p><strong>Pathname : </strong> {location.pathname}</p>
+    <p><strong>Hash : </strong> {location.hash}</p>
+    <p><strong>Search : </strong>{location.search}</p>
+    <p><strong>Mask : </strong>{location.mask}</p>
+    <p><strong>State : </strong>{location.state}</p>
+    <p><strong>Key : </strong>{location.key}</p>
+    </>
   )
 }
